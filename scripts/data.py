f1f247af5498b8b8fd8f10d24e0a1c7c0b9731b1
Alec Graves <shadysource2@gmail.com>
2018-01-29 08:22:25 -0500

    finalize data preprocessing tool

--- scripts/data.py
+++ scripts/data.py
@@ -8,41 +8,15 @@ from os.path import join
 import cv2
 import numpy as np
 
-input_data_dir = 'D:\\data\\road_detector\\zipped_files\\AOI_4_Shanghai_Roads_Train\\MUL-PanSharpen'
-# input_data_dir = 'D:\\data\\road_detector\\zipped_files\\AOI_4_Shanghai_Roads_Train\\RGB-PanSharpen'
-label_data_dir = 'D:\\data\\road_detector\\image_tools\\AOI_4_Shanghai_Masks'
-output_data_dir = 'D:\\data\\road_detector\\image_data'
-
-# for my files, the last integer in the filename is the data image number.
-name_key = lambda x : int(''.join([s for s in x[x.find('img'):] if s.isdigit()]))
-
-
-if not os.path.exists(output_data_dir):
-    os.makedirs(output_data_dir)
-
-input_files = os.listdir(input_data_dir)
-input_keys = [name_key(f) for f in input_files]
-label_files = os.listdir(label_data_dir)
-
-# my files are fucked, so sorting will not work.
-# input_files.sort(key=name_key)
-# label_files.sort(key=name_key)
-
-# print(len(input_files))
-# print(len(label_files))
-
-# print(input_files[-2])
-# print(label_files[-2])
-
 def preprocess_image(x, bits=8):
-    x = x.astype(np.float32)
+    x = x.astype(np.float16) # 16 bit for storage space
     x = np.divide(x, 2**bits) 
     x = np.subtract(x, 1.0) 
     x = np.multiply(x, 2.0) 
     return x
 
 def unprocess_image(x, bits=8):
-    x = x.astype(np.float32)
+    x = x.astype(np.float16)
     x = np.divide(x, 2.0)
     x = np.add(x, 1.0)
     x = np.multiply(x, 2**bits)
@@ -54,28 +28,59 @@ def unprocess_image(x, bits=8):
         ValueError('bits must be 8 or 16')
     return x
 
-for label_file in label_files:
-    input_file_idx = input_keys.index(name_key(label_file))
-    input_file = input_files[input_file_idx]
-    assert os.path.exists(join(input_data_dir, input_file))
-    x = cv2.imread(join(input_data_dir, input_file), cv2.IMREAD_UNCHANGED)
-    if x.dtype == 'uint8':
-        bits = 8
-    elif x.dtype == 'uint16':
-        bits = 16
-    x *= np.uint16(2**bits/np.max(np.max(x, axis=0), axis=0)) # expand image to use full channel range
-    x = preprocess_image(x, bits=bits)
-    
-    # Show image layers and exit:
-    x = unprocess_image(x, 8)
-    for i in range(x.shape[-1]):
-        cv2.imshow('image', x[:, :, i])
-        cv2.waitKey(2000)
-    exit()
+# for my files, the last integer in the filename is the data image number.
+name_key = lambda x : int(''.join([s for s in x[x.find('img'):] if s.isdigit()]))
 
-    y = cv2.imread(join(label_data_dir, label_file), cv2.IMREAD_GRAYSCALE)
-    # cv2.imshow('label', y)
+output_data_dir = 'D:\\data\\road_detector\\image_data'
+
+input_data_dirs = ['D:\\data\\road_detector\\zipped_files\\AOI_4_Shanghai_Roads_Train\\MUL-PanSharpen',
+                   'D:\\data\\road_detector\\zipped_files\\AOI_4_Shanghai_Roads_Train\\RGB-PanSharpen']
+label_data_dirs = ['D:\\data\\road_detector\\image_tools\\AOI_4_Shanghai_Masks',
+                    'D:\\data\\road_detector\\image_tools\\AOI_4_Shanghai_Roads_Nodes']
+
+if not os.path.exists(output_data_dir):
+    os.makedirs(output_data_dir)
+
+input_file_lists = [os.listdir(input_data_dir) for input_data_dir in input_data_dirs]
+input_keys_lists = [[name_key(f) for f in input_files] for input_files in input_file_lists]
+
+label_files_lists = [os.listdir(label_data_dir) for label_data_dir in label_data_dirs]
+label_keys_lists = [[name_key(f) for f in label_files] for label_files in label_files_lists]
+
+
+dataset_name = os.path.basename(label_data_dirs[0])
+for j, label_file in enumerate(label_files_lists[0]):
+    img_number = label_keys_lists[0][j]
+    try:
+        input_file_idx = [input_keys.index(img_number) for input_keys in input_keys_lists]
+    except ValueError: # input image_number is not in the list of files
+        continue
+    input_files = [input_file_lists[i][input_file_idx[i]] for i in range(len(input_file_idx))]
+    x = [cv2.imread(join(input_data_dirs[i], input_files[i]), cv2.IMREAD_UNCHANGED) for i in range(len(input_file_idx))]
+    
+    for i, x_i in enumerate(x):
+        if x_i.dtype == 'uint8':
+            bits = 8
+        elif x_i.dtype == 'uint16':
+            bits = 16
+        x_i *= np.uint16(2**bits/np.max(np.max(x_i, axis=0), axis=0)) # expand image to use full channel range
+        x_i = preprocess_image(x_i, bits=bits)
+        x[i] = x_i
+    x = np.concatenate(x, axis=-1)
+
+    # Show image layers and exit:
+    # x = unprocess_image(x, 8)
+    # for i in range(x.shape[-1]):
+    #     cv2.imshow('image', x[:, :, i])
+    #     cv2.waitKey(2000)
+    # exit()
+    label_file_idx = [label_keys.index(img_number) for label_keys in label_keys_lists]
+    label_files = [label_files_lists[i][label_file_idx[i]] for i in range(len(label_file_idx))]
+    labels = [np.expand_dims(cv2.imread(join(label_data_dirs[i], label_files[i]), cv2.IMREAD_GRAYSCALE), axis=-1) for i in range(len(label_file_idx))]
+    labels = np.concatenate(labels, axis=-1)
+    # cv2.imshow('label', labels)
     # cv2.waitKey(10000)
     # exit()
-    print(x.shape, y.shape)
-    exit()
+    print('saving as', join(output_data_dir, dataset_name + str(img_number)), '...')
+    np.savez_compressed(join(output_data_dir, dataset_name + str(img_number)), x=x, y=labels)
+    print(x.shape, labels.shape)
