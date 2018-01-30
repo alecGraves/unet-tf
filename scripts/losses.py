7df9785d143ede4f5f0974bbfe3764f8e1c7f75e
Alec Graves <shadysource2@gmail.com>
2018-01-29 21:02:07 -0500

    switch to built in iou

--- scripts/losses.py
+++ scripts/losses.py
@@ -4,6 +4,8 @@ def iou_loss(y_true, y_pred):
     '''
     computes the IOU loss for a group of labels
     '''
+    return tf.metrics.mean_iou(y_true, y_pred, 2)
+
     # flatten labels:
     y_true = tf.reshape(y_true, [-1])
     y_pred = tf.reshape(y_pred, [-1])
