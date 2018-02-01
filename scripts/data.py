25c0a53d444278c336b1d585efd3208c2db0acd4
stoplime <slanimero@gmail.com>
2018-02-01 17:49:58 -0500

    update data

--- scripts/data.py
+++ scripts/data.py
@@ -7,27 +7,21 @@ import os
 from os.path import join
 import cv2
 import numpy as np
+from PIL import Image
 
-datamax = [   10,    10,    10,    10,  2000, 2000, 2000]
-# datamean =  [ 0.01271057,  0.01102448,  0.00970459,  0.01496124,  0.00893402,  0.01432037,  0.01338959]
-# datastd  = [ 0.15515137,  0.10876465 , 0.10717773,  0.15661621,  0.15698242,  0.19836426,  0.13061523]
-# datamean = [1.56652696, 1.09011534,    1.07365105,    1.59740709,  314.23654353,  398.08631865,  262.84795669]
-# datastd =  [  0.1086813,    0.09061887,   0.07617772,   0.12855516,  16.98786878,  26.78131989,  23.9744957 ]
-datamean = [ 0.11956787,  0.08380127 , 0.08282471 , 0.11938477 , 0.12207031,  0.15380859,  0.10076904]
-datastd = [ 0.021698,    0.01623535,  0.01527405,  0.02445984 , 0.01235962 , 0.01832581,  0.01791382]
-
-def preprocess_image(x, mean=None, std=None):
+def preprocess_image(x):
     x = x.astype(np.float64)
-    x = x/np.array(datamax).reshape(1, 1, x.shape[-1])
-    x = (x - 0.5+np.array(datamean).reshape(1,1,x.shape[-1])) / (0.5+np.array(datastd).reshape(1,1,x.shape[-1]))
-    # print(np.min(np.min(x, axis=0), axis=0), np.max(np.max(x, axis=0), axis=0))
-    x[x > 1.] = 1.
-    x[x < -1.] = -1.
+    channelmax = np.asarray(np.max(np.max(x, axis=0), axis=0))
+    channelmax[channelmax < 1] = 1000
+    x = x / channelmax
+    x = x - 0.5
+    x = x * 2.
     return x.astype(np.float16)
 
-def unprocess_image(x, mean=None, std=None, bits=8):
-    x = (x) * np.array(datastd).reshape(1,1,x.shape[-1]) + np.array(datamean).reshape(1,1,x.shape[-1])
-    x = np.multiply(x, 2**bits-1)
+def unprocess_image(x):
+    x = x / 2
+    x = x + 0.
+    x = x * 255
     x = x.astype(np.uint8)
     return x
 
@@ -48,13 +42,14 @@ if __name__ == "__main__":
     output_data_dir = '/home/marvin/road_detector/train'
     input_data = '/home/marvin/road_detector/unzipped/inputs'
     labels_data = '/home/marvin/road_detector/unzipped/labels/'
+    reshape_size = 512
 
     for _dir in os.listdir(input_data):
 
-        input_data_dirs = [join(input_data, _dir, 'MUL-PanSharpen'),
-                            join(input_data,_dir,'RGB-PanSharpen')]
-        label_data_dirs = [join(labels_data, _dir[:-12]+ '_Masks'),
-                            join(labels_data, _dir[:-12]+ '_Roads_Nodes')]
+        input_data_dirs = [join(input_data, _dir, 'RGB-PanSharpen')]#,
+                            # join(input_data,_dir,'RGB-PanSharpen')]
+        label_data_dirs = [join(labels_data, _dir[:-12]+ '_Masks')]#,
+                            # join(labels_data, _dir[:-12]+ '_Roads_Nodes')]
 
         if not os.path.exists(output_data_dir):
             os.makedirs(output_data_dir)
@@ -77,10 +72,12 @@ if __name__ == "__main__":
                 continue
             input_files = [input_file_lists[i][input_file_idx[i]] for i in range(len(input_file_idx))]
             image = [cv2.imread(join(input_data_dirs[i], input_files[i]), cv2.IMREAD_UNCHANGED) for i in range(len(input_file_idx))]
-            
+            image = [np.array(Image.fromarray(unprocess_image(preprocess_image(img))).resize((reshape_size, reshape_size))) for img in image]
             image = np.concatenate(image, axis=-1)
+            # print(image.shape)
             image = preprocess_image(image)
-
+            # Image.fromarray(unprocess_image(image)).show()
+            
 
             # Show image layers and exit:
             # image = unprocess_image(image, 8)
@@ -94,9 +91,13 @@ if __name__ == "__main__":
 
             label_file_idx = [label_keys.index(img_number) for label_keys in label_keys_lists]
             label_files = [label_files_lists[i][label_file_idx[i]] for i in range(len(label_file_idx))]
-            labels = [np.expand_dims(cv2.imread(join(label_data_dirs[i], label_files[i]), cv2.IMREAD_GRAYSCALE), axis=-1) for i in range(len(label_file_idx))]
+            labels = [cv2.imread(join(label_data_dirs[i], label_files[i]), cv2.IMREAD_GRAYSCALE) for i in range(len(label_file_idx))]
+            labels = [np.array(Image.fromarray(img).resize((reshape_size, reshape_size))) for img in labels]
             labels = np.concatenate(labels, axis=-1)
-            labels = preprocess_label(labels)
+            # print(labels.shape)
+            labels = np.expand_dims(preprocess_label(labels), axis=-1)
+            # Image.fromarray(unprocess_label(labels)).show()
+            # exit()
             # for i in range(labels.shape[-1]):
             #     # cv2.imshow('label', labels)
             #     # cv2.waitKey(10000)
@@ -114,7 +115,7 @@ if __name__ == "__main__":
 
             x.append(image)
             y.append(labels)
-            if j%100==0 and j > 0: # about 3 gigs uncompressed
+            if j%500==0 and j > 0: # about 3 gigs uncompressed
                 x = np.array(x)
                 y = np.array(y)
                 print('x', x.shape, 'y', y.shape)
